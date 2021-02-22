840e8ef2b72e8fafccb35a995402823f3a71fba9
Avinash Kumar <bhaiavinash9@gmail.com>
2021-02-23 01:38:54 +0530

    done

--- monumento_module/lib/utils/popular_carousel.dart
+++ monumento_module/lib/utils/popular_carousel.dart
@@ -14,176 +14,135 @@ class PopularMonumentsCarousel extends StatelessWidget {
   Widget build(BuildContext context) {
     final List<Widget> imageSliders = popMonumentDocs
         .map((item) => GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (_) => DetailScreen(
-                                  monument: item,
-                                  user: user,
-                                  isBookMarked: false,
-                                )));
-                  },
-                  child: Container(
-                    margin: EdgeInsets.all(10.0),
-                    width: 210.0,
-                    child: Stack(
-                      alignment: Alignment.topCenter,
-                      children: <Widget>[
-                        Positioned(
-                          bottom: 15.0,
-                          child: Container(
-                            height: 120.0,
-                            width: 200.0,
-                            decoration: BoxDecoration(
-                              color: Colors.white,
-                              borderRadius: BorderRadius.circular(10.0),
-                              boxShadow: [
-                                BoxShadow(
-                                  color: Colors.black26,
-                                  offset: Offset(0.0, 2.0),
-                                  blurRadius: 6.0,
-                                ),
-                              ],
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (_) => DetailScreen(
+                              monument: item,
+                              user: user,
+                              isBookMarked: false,
+                            )));
+              },
+              child: Container(
+                margin: EdgeInsets.all(10.0),
+                width: 210.0,
+                child: Stack(
+                  alignment: Alignment.topCenter,
+                  children: <Widget>[
+                    Positioned(
+                      bottom: 15.0,
+                      child: Container(
+                        height: 120.0,
+                        width: 200.0,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10.0),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black26,
+                              offset: Offset(0.0, 2.0),
+                              blurRadius: 6.0,
                             ),
-                            child: Padding(
-                              padding: EdgeInsets.all(10.0),
-                              child: Column(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceAround,
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: <Widget>[
-                                  Padding(
-                                    padding: const EdgeInsets.only(top: 20.0),
-                                    child: Text(
-                                      item.data['name'] ?? 'Monument',
-                                      style: TextStyle(
-                                        fontSize: 21.0,
-                                        fontWeight: FontWeight.w600,
-                                        letterSpacing: 1.2,
-                                      ),
-                                    ),
-                                  ),
-                                  Text(
-                                    'Tap to Explore',
-                                    textAlign: TextAlign.center,
-                                    style: TextStyle(
-                                        color: Colors.amber,
-                                        fontSize: 15.0,
-                                        fontWeight: FontWeight.w600),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ),
+                          ],
                         ),
-                        Container(
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(20.0),
-                            boxShadow: [
-                              BoxShadow(
-                                color: Colors.black26,
-                                offset: Offset(0.0, 2.0),
-                                blurRadius: 6.0,
+                        child: Padding(
+                          padding: EdgeInsets.all(10.0),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Padding(
+                                padding: const EdgeInsets.only(top: 20.0),
+                                child: Text(
+                                  item.data['name'] ?? 'Monument',
+                                  style: TextStyle(
+                                    fontSize: 21.0,
+                                    fontWeight: FontWeight.w600,
+                                    letterSpacing: 1.2,
+                                  ),
+                                ),
+                              ),
+                              Text(
+                                'Tap to Explore',
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                    color: Colors.amber,
+                                    fontSize: 15.0,
+                                    fontWeight: FontWeight.w600),
                               ),
                             ],
                           ),
-                          child: Stack(
-                            children: <Widget>[
-                              Hero(
-                                tag: item.data['name'] ?? 'monument',
-                                child: ClipRRect(
-                                  borderRadius: BorderRadius.circular(20.0),
-                                  child: Image(
-                                    height: 180.0,
-                                    width: 180.0,
-                                    image: NetworkImage(item.data['image']),
-                                    fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(20.0),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black26,
+                            offset: Offset(0.0, 2.0),
+                            blurRadius: 6.0,
+                          ),
+                        ],
+                      ),
+                      child: Stack(
+                        children: <Widget>[
+                          Hero(
+                            tag: item.data['name'] ?? 'monument',
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(20.0),
+                              child: Image(
+                                height: 180.0,
+                                width: 180.0,
+                                image: NetworkImage(item.data['image']),
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                          Positioned(
+                            left: 10.0,
+                            bottom: 10.0,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Text(
+                                  item.data['city'] ?? 'City',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 24.0,
+                                    fontWeight: FontWeight.w600,
+                                    letterSpacing: 1.2,
                                   ),
                                 ),
-                              ),
-                              Positioned(
-                                left: 10.0,
-                                bottom: 10.0,
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                Row(
                                   children: <Widget>[
+                                    Icon(
+                                      Icons.location_on,
+                                      size: 10.0,
+                                      color: Colors.white,
+                                    ),
+                                    SizedBox(width: 5.0),
                                     Text(
-                                      item.data['city'] ?? 'City',
+                                      item.data['country'] ?? 'Country',
                                       style: TextStyle(
                                         color: Colors.white,
-                                        fontSize: 24.0,
-                                        fontWeight: FontWeight.w600,
-                                        letterSpacing: 1.2,
                                       ),
-                                    ),
-                                    Row(
-                                      children: <Widget>[
-                                        Icon(
-                                          Icons.location_on,
-                                          size: 10.0,
-                                          color: Colors.white,
-                                        ),
-                                        SizedBox(width: 5.0),
-                                        Text(
-                                          item.data['country'] ?? 'Country',
-                                          style: TextStyle(
-                                            color: Colors.white,
-                                          ),
-                                        ),
-                                      ],
                                     ),
                                   ],
                                 ),
-                              ),
-                            ],
+                              ],
+                            ),
                           ),
-                        )
-                      ],
-                    ),
-                  ),
-                )
-            /*Container(
-  child: Container(
-    margin: EdgeInsets.all(5.0),
-    child: ClipRRect(
-      borderRadius: BorderRadius.all(Radius.circular(5.0)),
-      child: Stack(
-        children: <Widget>[
-          Image.network(item, fit: BoxFit.cover, width: 1000.0),
-          Positioned(
-            bottom: 0.0,
-            left: 0.0,
-            right: 0.0,
-            child: Container(
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  colors: [
-                    Color.fromARGB(200, 0, 0, 0),
-                    Color.fromARGB(0, 0, 0, 0)
+                        ],
+                      ),
+                    )
                   ],
-                  begin: Alignment.bottomCenter,
-                  end: Alignment.topCenter,
                 ),
               ),
-              padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-              child: Text(
-                'No. ${imgList.indexOf(item)} image',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-          ),
-        ],
-      )
-    ),
-  ),
-)*/
-            )
+            ))
         .toList();
     return Column(
       children: <Widget>[
@@ -229,148 +188,6 @@ class PopularMonumentsCarousel extends StatelessWidget {
                 enlargeStrategy: CenterPageEnlargeStrategy.height),
             items: imageSliders,
           ),
-          /*ListView.builder(
-            scrollDirection: Axis.horizontal,
-            itemCount: popMonumentDocs.length,
-            itemBuilder: (BuildContext context, int index) {
-              return GestureDetector(
-                onTap: () {
-                  Navigator.push(context,
-                  MaterialPageRoute(builder: (_) => DetailScreen(
-                    monument: popMonumentDocs[index],
-                  user: user,
-                  isBookMarked: false,
-                  ))
-                  );
-                },
-                child: Container(
-                  margin: EdgeInsets.all(10.0),
-                  width: 210.0,
-                  child: Stack(
-                    alignment: Alignment.topCenter,
-                    children: <Widget>[
-                      Positioned(
-                        bottom: 15.0,
-                        child: Container(
-                          height: 120.0,
-                          width: 200.0,
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(10.0),
-                            boxShadow: [
-                              BoxShadow(
-                                color: Colors.black26,
-                                offset: Offset(0.0, 2.0),
-                                blurRadius: 6.0,
-                              ),
-                            ],
-                          ),
-                          child: Padding(
-                            padding: EdgeInsets.all(10.0),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.spaceAround,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: <Widget>[
-                                Padding(
-                                  padding: const EdgeInsets.only(top: 20.0),
-                                  child: Text(
-                                    popMonumentDocs[index].data['name'] ??
-                                        'Monument',
-                                    style: TextStyle(
-                                      fontSize: 21.0,
-                                      fontWeight: FontWeight.w600,
-                                      letterSpacing: 1.2,
-                                    ),
-                                  ),
-                                ),
-                                Text(
-                                    'Tap to Explore',
-                                    textAlign: TextAlign.center,
-                                    style: TextStyle(
-                                      color: Colors.amber,
-                                      fontSize: 15.0,
-                                      fontWeight: FontWeight.w600
-                                    ),
-                                  ),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ),
-                      Container(
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(20.0),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.black26,
-                              offset: Offset(0.0, 2.0),
-                              blurRadius: 6.0,
-                            ),
-                          ],
-                        ),
-                        child: Stack(
-                          children: <Widget>[
-                            Hero(
-                              tag: popMonumentDocs[index].data['name'] ??
-                                  'monument',
-                              child: ClipRRect(
-                                borderRadius: BorderRadius.circular(20.0),
-                                child: Image(
-                                  height: 180.0,
-                                  width: 180.0,
-                                  image: NetworkImage(
-                                      popMonumentDocs[index].data['image']),
-                                  fit: BoxFit.cover,
-                                ),
-                              ),
-                            ),
-                            Positioned(
-                              left: 10.0,
-                              bottom: 10.0,
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: <Widget>[
-                                  Text(
-                                    popMonumentDocs[index].data['city'] ??
-                                        'City',
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 24.0,
-                                      fontWeight: FontWeight.w600,
-                                      letterSpacing: 1.2,
-                                    ),
-                                  ),
-                                  Row(
-                                    children: <Widget>[
-                                      Icon(
-                                        Icons.location_on,
-                                        size: 10.0,
-                                        color: Colors.white,
-                                      ),
-                                      SizedBox(width: 5.0),
-                                      Text(
-                                        popMonumentDocs[index]
-                                                .data['country'] ??
-                                            'Country',
-                                        style: TextStyle(
-                                          color: Colors.white,
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ],
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-              );
-            },
-          ),*/
         ),
       ],
     );
