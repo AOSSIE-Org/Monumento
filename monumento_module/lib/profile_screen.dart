aa45474ccc224071b90b717e9768c01a2128b1b3
Avinash Kumar <bhaiavinash9@gmail.com>
2021-02-24 00:02:47 +0530

    fix:update profile pic

--- monumento_module/lib/profile_screen.dart
+++ monumento_module/lib/profile_screen.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:monumento/login_screen.dart';
+import 'package:monumento/utils/bottomsheet.dart';
 
 class UserProfilePage extends StatefulWidget {
   final FirebaseUser user;
@@ -46,15 +47,15 @@ class _UserProfilePageState extends State<UserProfilePage> {
 
   Widget _buildProfileImage() {
     return Center(
-      child: Container(
+        child: Stack(alignment: Alignment.bottomRight, children: [
+      Container(
         width: 140.0,
         height: 140.0,
         decoration: BoxDecoration(
           image: DecorationImage(
-            image: widget.profileSnapshot.data['prof_pic'] != ''?
-            NetworkImage(widget.profileSnapshot.data['prof_pic'])
-            :
-            AssetImage('assets/explore.jpg'),
+            image: widget.profileSnapshot.data['prof_pic'] != ''
+                ? NetworkImage(widget.profileSnapshot.data['prof_pic'])
+                : AssetImage('assets/explore.jpg'),
             fit: BoxFit.cover,
           ),
           borderRadius: BorderRadius.circular(80.0),
@@ -64,7 +65,29 @@ class _UserProfilePageState extends State<UserProfilePage> {
           ),
         ),
       ),
-    );
+      Positioned(
+        child: InkWell(
+          onTap: () async {
+            await showBottom(context, widget.profileSnapshot);
+          },
+          child: Container(
+              width: 50.0,
+              height: 50.0,
+              decoration: BoxDecoration(
+                color: Colors.amber,
+                borderRadius: BorderRadius.circular(25.0),
+                border: Border.all(
+                  color: Colors.white,
+                ),
+              ),
+              child: Center(
+                  child: Icon(
+                Icons.add_a_photo,
+                color: Colors.white,
+              ))),
+        ),
+      )
+    ]));
   }
 
   Widget _buildFullName() {
@@ -75,7 +98,7 @@ class _UserProfilePageState extends State<UserProfilePage> {
     );
 
     return Text(
-      widget.profileSnapshot.data["name"]??"Monumento User", //_fullName
+      widget.profileSnapshot.data["name"] ?? "Monumento User", //_fullName
       style: _nameTextStyle,
     );
   }
@@ -88,8 +111,9 @@ class _UserProfilePageState extends State<UserProfilePage> {
         borderRadius: BorderRadius.circular(4.0),
       ),
       child: Text(
-        widget.profileSnapshot.data["status"] == ''?'Monumento-nian':
-        widget.profileSnapshot.data["status"],
+        widget.profileSnapshot.data["status"] == ''
+            ? 'Monumento-nian'
+            : widget.profileSnapshot.data["status"],
         style: TextStyle(
           fontFamily: 'Spectral',
           color: Colors.black,
@@ -110,18 +134,16 @@ class _UserProfilePageState extends State<UserProfilePage> {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: <Widget>[
-          Text('Bookmarks: ',
-            style: TextStyle(
-                color: Colors.black,
-                fontSize: 22.0
-            ),
+          Text(
+            'Bookmarks: ',
+            style: TextStyle(color: Colors.black, fontSize: 22.0),
           ),
-          Text(widget.bookmarkedMonuments.length.toString(),
+          Text(
+            widget.bookmarkedMonuments.length.toString(),
             style: TextStyle(
                 color: Colors.amber,
                 fontSize: 24.0,
-                fontWeight: FontWeight.bold
-            ),
+                fontWeight: FontWeight.bold),
           ),
         ],
       ),
@@ -131,7 +153,7 @@ class _UserProfilePageState extends State<UserProfilePage> {
   Widget _buildBio(BuildContext context) {
     TextStyle bioTextStyle = TextStyle(
       fontFamily: 'Spectral',
-      fontWeight: FontWeight.w400,//try changing weight to w500 if not thin
+      fontWeight: FontWeight.w400, //try changing weight to w500 if not thin
       fontStyle: FontStyle.italic,
       color: Color(0xFF799497),
       fontSize: 16.0,
@@ -163,8 +185,7 @@ class _UserProfilePageState extends State<UserProfilePage> {
       padding: EdgeInsets.only(top: 8.0),
       child: Text(
         "Last Visited:",
-        style: TextStyle(
-            fontSize: 16.0),
+        style: TextStyle(fontSize: 16.0),
       ),
     );
   }
@@ -177,69 +198,87 @@ class _UserProfilePageState extends State<UserProfilePage> {
       body: Stack(
         children: <Widget>[
           _buildCoverImage(screenSize, context),
-          (widget.profileSnapshot == null || widget.profileSnapshot.data == null)?
-          Center(
-            child: Container(
-              height: 50.0,
-              width: 50.0,
-              child: CircularProgressIndicator(
-                valueColor: AlwaysStoppedAnimation<Color>(Colors.amber),
-              ),
-            ),
-          ):
-    SafeArea(
-            child: SingleChildScrollView(
-              child: Column(
-                children: <Widget>[
-                  SizedBox(height: screenSize.height / 6.4),
-                  _buildProfileImage(),
-                  _buildFullName(),
-                  _buildStatus(context),
-                  _buildStatContainer(),
-                  _buildBio(context),
-                  _buildSeparator(screenSize),
-                  SizedBox(height: 8.0),
-                  widget.bookmarkedMonuments.length != 0?
-                  _buildGetInTouch(context):SizedBox.shrink(),
-                  SizedBox(height: 4.0),
-                  widget.bookmarkedMonuments.length > 0?
-                  ListTile(
-                    title: Text(widget.bookmarkedMonuments[0].data['name']),
-                    leading: Icon(Icons.account_balance, color: Colors.amber,),
-                    dense: true,
-                  ):SizedBox.shrink(),
-                  widget.bookmarkedMonuments.length > 1?
-                  ListTile(
-                    title: Text(widget.bookmarkedMonuments[1].data['name']),
-                    leading: Icon(Icons.account_balance, color: Colors.amber,),
-                    dense: true,
-                  ):SizedBox.shrink(),
-                  MaterialButton(
-                    color: Colors.red,
-                    padding: EdgeInsets.all(4.0),
-                    child: Text('Log Out', textAlign: TextAlign.center, style: TextStyle(color: Colors.white),),
-                    onPressed: () async{
-                      _key.currentState.showSnackBar(SnackBar(
-                        backgroundColor: Colors.amber,
-                        content: Text('Logging Out!',
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      ));
-                      await FirebaseAuth.instance.signOut().whenComplete(() {
-                        Navigator.pushReplacement(context,
-                            MaterialPageRoute(builder: (_) => LoginScreen())
-                        );
-                      });
-                    },
-                  )
-                ],
-              ),
-            ),
-          ),
+          (widget.profileSnapshot == null ||
+                  widget.profileSnapshot.data == null)
+              ? Center(
+                  child: Container(
+                    height: 50.0,
+                    width: 50.0,
+                    child: CircularProgressIndicator(
+                      valueColor: AlwaysStoppedAnimation<Color>(Colors.amber),
+                    ),
+                  ),
+                )
+              : SafeArea(
+                  child: SingleChildScrollView(
+                    child: Column(
+                      children: <Widget>[
+                        SizedBox(height: screenSize.height / 6.4),
+                        _buildProfileImage(),
+                        _buildFullName(),
+                        _buildStatus(context),
+                        _buildStatContainer(),
+                        _buildBio(context),
+                        _buildSeparator(screenSize),
+                        SizedBox(height: 8.0),
+                        widget.bookmarkedMonuments.length != 0
+                            ? _buildGetInTouch(context)
+                            : SizedBox.shrink(),
+                        SizedBox(height: 4.0),
+                        widget.bookmarkedMonuments.length > 0
+                            ? ListTile(
+                                title: Text(
+                                    widget.bookmarkedMonuments[0].data['name']),
+                                leading: Icon(
+                                  Icons.account_balance,
+                                  color: Colors.amber,
+                                ),
+                                dense: true,
+                              )
+                            : SizedBox.shrink(),
+                        widget.bookmarkedMonuments.length > 1
+                            ? ListTile(
+                                title: Text(
+                                    widget.bookmarkedMonuments[1].data['name']),
+                                leading: Icon(
+                                  Icons.account_balance,
+                                  color: Colors.amber,
+                                ),
+                                dense: true,
+                              )
+                            : SizedBox.shrink(),
+                        MaterialButton(
+                          color: Colors.red,
+                          padding: EdgeInsets.all(4.0),
+                          child: Text(
+                            'Log Out',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          onPressed: () async {
+                            _key.currentState.showSnackBar(SnackBar(
+                              backgroundColor: Colors.amber,
+                              content: Text(
+                                'Logging Out!',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ));
+                            await FirebaseAuth.instance
+                                .signOut()
+                                .whenComplete(() {
+                              Navigator.pushReplacement(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (_) => LoginScreen()));
+                            });
+                          },
+                        )
+                      ],
+                    ),
+                  ),
+                ),
         ],
       ),
     );
   }
 }
-
-
