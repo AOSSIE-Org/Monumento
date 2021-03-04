cee3be0e3d59d2b265a94f152df2e97125609cc9
Avinash Kumar <bhaiavinash9@gmail.com>
2021-03-04 13:25:04 +0530

    resolved conflits

--- monumento_module/lib/bookmark_screen.dart
+++ monumento_module/lib/bookmark_screen.dart
@@ -26,136 +26,138 @@ class _BookmarkScreenState extends State<BookmarkScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        elevation: 0.0,
-        title: Text(
-          'Explore Bookmark Monuments',
-          style: TextStyle(
-              fontSize: 19.0, fontWeight: FontWeight.bold, color: Colors.amber),
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          elevation: 0.0,
+          title: Text(
+            'Explore Bookmark Monuments',
+            style: TextStyle(
+                fontSize: 19.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.amber),
+          ),
         ),
-      ),
+        body: FutureBuilder(
+            future: getBookmarkedMonuments(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting)
+                return Center(child: CircularProgressIndicator());
+              if (!snapshot.hasData || snapshot.data.length <= 0)
+                return Container(
+                  child: Center(
+                      child: Text(
+                    'No Bookmarks!',
+                    style:
+                        TextStyle(fontWeight: FontWeight.w700, fontSize: 26.0),
+                  )),
+                );
 
-      body: FutureBuilder(
-          future: getBookmarkedMonuments(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting)
-              return Center(child: CircularProgressIndicator());
-            if (!snapshot.hasData || snapshot.data.length <= 0)
-              return Container(
-                child: Center(
-                    child: Text(
-                  'No Bookmarks!',
-                  style: TextStyle(fontWeight: FontWeight.w700, fontSize: 26.0),
-                )),
-              );
-
-            widget.monumentList = snapshot.data;
-            return ListView.builder(
-              scrollDirection: Axis.vertical,
-              itemCount: widget.monumentList.length,
-              itemBuilder: (BuildContext context, int index) {
-                return InkWell(
-                  onTap: () {
-                    print(widget.monumentList);
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (_) => DetailScreen(
-                                  user: widget.user,
-                                  monument: widget.monumentList[index],
-                                  isBookMarked: true,
-                                )));
-                  },
-                  child: Card(
-                    margin: EdgeInsets.all(15.0),
-                    elevation: 10.0,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20.0),
-                    ),
-                    child: Container(
-                      height: MediaQuery.of(context).size.height * 0.2,
-                      child: Row(
-                        children: <Widget>[
-                          Hero(
-                              tag: widget.monumentList[index].data['wiki'] ??
-                                  'monument-tag',
-                              child: Container(
+              widget.monumentList = snapshot.data;
+              return ListView.builder(
+                  scrollDirection: Axis.vertical,
+                  itemCount: widget.monumentList.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return InkWell(
+                        onTap: () {
+                          print(widget.monumentList);
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (_) => DetailScreen(
+                                        user: widget.user,
+                                        monument: widget.monumentList[index],
+                                        isBookMarked: true,
+                                      )));
+                        },
+                        child: Card(
+                            margin: EdgeInsets.all(15.0),
+                            elevation: 10.0,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20.0),
+                            ),
+                            child: Container(
                                 height:
                                     MediaQuery.of(context).size.height * 0.2,
-                                width: MediaQuery.of(context).size.width * 0.3,
-                                decoration: BoxDecoration(
-                                    color: Colors.grey,
-                                    borderRadius: BorderRadius.only(
-                                        topLeft: Radius.circular(20.0),
-                                        bottomLeft: Radius.circular(20.0)),
-                                    image: DecorationImage(
-                                        image: NetworkImage(widget
-                                            .monumentList[index].data['image']),
-                                        fit: BoxFit.cover)),
-                              )),
-                          SizedBox(
-                            width: 10.0,
-                          ),
-
-                          Container(
-                          Expanded(
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.spaceAround,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: <Widget>[
-                                Text(
-                                  widget.monumentList[index].data['name'],
-                                  style: TextStyle(
-                                      fontSize: 20.0,
-                                      fontWeight: FontWeight.w700),
-                                ),
-                                Container(
-                                  width:
-                                      MediaQuery.of(context).size.width * 0.57,
-                                  maxLines: 2,
-                                  overflow: TextOverflow.ellipsis,
-                                ),
-                                Container(
-                                  child: Text(
-                                    widget.monumentList[index].data['city'] +
-                                        ', ' +
-                                        widget.monumentList[index]
-                                            .data['country'],
-                                    maxLines: 3,
-                                    style: TextStyle(
-                                      fontSize: 18.0,
-                                    ),
-                                    overflow: TextOverflow.ellipsis,
+                                child: Row(children: <Widget>[
+                                  Hero(
+                                      tag: widget.monumentList[index]
+                                              .data['wiki'] ??
+                                          'monument-tag',
+                                      child: Container(
+                                        height:
+                                            MediaQuery.of(context).size.height *
+                                                0.2,
+                                        width:
+                                            MediaQuery.of(context).size.width *
+                                                0.3,
+                                        decoration: BoxDecoration(
+                                            color: Colors.grey,
+                                            borderRadius: BorderRadius.only(
+                                                topLeft: Radius.circular(20.0),
+                                                bottomLeft:
+                                                    Radius.circular(20.0)),
+                                            image: DecorationImage(
+                                                image: NetworkImage(widget
+                                                    .monumentList[index]
+                                                    .data['image']),
+                                                fit: BoxFit.cover)),
+                                      )),
+                                  SizedBox(
+                                    width: 10.0,
                                   ),
-                                ),
-                                Row(
-                                  children: <Widget>[
-                                    Text(
-                                      'Explore more',
-                                      style: TextStyle(
-                                          color: Colors.amber,
-                                          fontWeight: FontWeight.bold),
-                                    ),
-                                    SizedBox(
-                                      width: 6.0,
-                                    ),
-                                    Icon(
-                                      Icons.chevron_right,
-                                      color: Colors.amber,
-                                    )
-                                  ],
-                                ),
-                              ],
-                            ),
-
-                          ),
-
-<<<<<<< monumento_module/lib/bookmark_screen.dart
-              },
-            );
-          }),
-
-    );
+                                  Container(
+                                      child: Expanded(
+                                          child: Column(
+                                              mainAxisAlignment:
+                                                  MainAxisAlignment.spaceAround,
+                                              crossAxisAlignment:
+                                                  CrossAxisAlignment.start,
+                                              children: <Widget>[
+                                        Text(
+                                          widget
+                                              .monumentList[index].data['name'],
+                                          style: TextStyle(
+                                              fontSize: 20.0,
+                                              fontWeight: FontWeight.w700),
+                                        ),
+                                        Container(
+                                          width: MediaQuery.of(context)
+                                                  .size
+                                                  .width *
+                                              0.57,
+                                          child: Text(
+                                            widget.monumentList[index]
+                                                    .data['city'] +
+                                                ', ' +
+                                                widget.monumentList[index]
+                                                    .data['country'],
+                                            maxLines: 3,
+                                            style: TextStyle(
+                                              fontSize: 18.0,
+                                            ),
+                                            overflow: TextOverflow.ellipsis,
+                                          ),
+                                        ),
+                                        Row(
+                                          children: <Widget>[
+                                            Text(
+                                              'Explore more',
+                                              style: TextStyle(
+                                                  color: Colors.amber,
+                                                  fontWeight: FontWeight.bold),
+                                            ),
+                                            SizedBox(
+                                              width: 6.0,
+                                            ),
+                                            Icon(
+                                              Icons.chevron_right,
+                                              color: Colors.amber,
+                                            )
+                                          ],
+                                        ),
+                                      ])))
+                                ]))));
+                  });
+            }));
   }
 }
