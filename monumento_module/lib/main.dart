2fe3a6f9cc4345e90433c3dcb34e12437bdcdfd0
piyushpradhan <piyushpradhan3.14@gmail.com>
2021-03-28 10:37:03 +0530

    code formatting

--- monumento_module/lib/main.dart
+++ monumento_module/lib/main.dart
@@ -22,10 +22,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setPreferredOrientations([
-      DeviceOrientation.portraitUp,
-      DeviceOrientation.portraitDown
-    ]);
+    SystemChrome.setPreferredOrientations(
+        [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
     return MaterialApp(
       title: 'Monumento',
       debugShowCheckedModeBanner: false,
