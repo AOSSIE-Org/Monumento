a721bb729081346c40e3a55513f2bc57d1a3f7d7
Mohammed Mohsin <59914433+mdmohsin7@users.noreply.github.com>
2024-06-09 20:40:52 +0530

    setup monument repository

---
+++ lib/data/repositories/firebase_monument_repository.dart
@@ -0,0 +1,56 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:monumento/data/models/bookmarked_monument_model.dart';
+import 'package:monumento/data/models/monument_model.dart';
+import 'package:monumento/data/models/user_model.dart';
+import 'package:monumento/data/models/wiki_data_model.dart';
+import 'package:monumento/domain/repositories/monument_repository.dart';
+import 'package:wikipedia/wikipedia.dart';
+
+class FirebaseMonumentRepository implements MonumentRepository {
+  FirebaseFirestore _database;
+
+  FirebaseMonumentRepository({FirebaseFirestore? database})
+      : _database = database ?? FirebaseFirestore.instance;
+
+  @override
+  Future<List<MonumentModel>> getPopularMonuments() async {
+    final docs = await _database.collection('monuments').get();
+    final List<MonumentModel> popularMonumentsDocs =
+        docs.docs.map((doc) => MonumentModel.fromJson(doc.data())).toList();
+    return popularMonumentsDocs;
+  }
+
+  @override
+  Stream<List<BookmarkedMonumentModel>> getBookmarkedMonuments(String userId) {
+    Stream<List<BookmarkedMonumentModel>> streamBookmarkedMonuments = _database
+        .collection('bookmarks')
+        .where("uid", isEqualTo: userId)
+        .snapshots()
+        .map((querySnap) => querySnap.docs
+            .map((doc) => BookmarkedMonumentModel.fromJson(doc.data()))
+            .toList());
+
+    return streamBookmarkedMonuments;
+  }
+
+  @override
+  Future<UserModel?> getProfileData(String userId) async {
+    final snap = await _database.collection('users').doc(userId).get();
+    if (snap.exists) {
+      return UserModel.fromJson(snap.data()!);
+    }
+    return null;
+  }
+
+  @override
+  Future<WikiDataModel> getMonumentWikiDetails(String wikiId) async {
+    Wikipedia instance = Wikipedia();
+    var res = await instance.searchSummaryWithPageId(pageId: int.parse(wikiId));
+    return WikiDataModel(
+      extract: res!.extract!,
+      title: res.title!,
+      description: res.description!,
+      pageId: wikiId,
+    );
+  }
+}
