60e681aee8022b2aba137d020359224fe7f382c6
Mohammed Mohsin <59914433+mdmohsin7@users.noreply.github.com>
2024-06-21 20:44:48 +0530

    add methods for posts, comments, likes & recommended users in social repository

--- lib/domain/repositories/social_repository.dart
+++ lib/domain/repositories/social_repository.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:monumento/data/models/comment_model.dart';
+import 'package:monumento/data/models/post_model.dart';
 import 'package:monumento/data/models/user_model.dart';
 
 abstract interface class SocialRepository {
@@ -15,4 +17,22 @@ abstract interface class SocialRepository {
       {required String searchQuery, required DocumentSnapshot startAfterDoc});
 
   Future<bool> checkUserNameAvailability({required String username});
+
+  Future<List<PostModel>> getInitialFeedPosts();
+
+  Future<List<PostModel>> getMorePosts({required String startAfterDocId});
+
+  Future<void> likePost({required String postId});
+
+  Future<void> unlikePost({required String postId});
+
+  Future<CommentModel> addNewComment(
+      {required String postDocId, required String comment});
+
+  Future<List<CommentModel>> getInitialComments({required String postDocId});
+
+  Future<List<CommentModel>> getMoreComments(
+      {required String postDocId, required String startAfterDocId});
+
+  Future<List<UserModel>> getRecommendedUsers();
 }
