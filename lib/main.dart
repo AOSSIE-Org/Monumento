5d953b2db2db1a7c82cf56b5118ef400069d54e6
kareem abdeen <kareem.elshreef18@gmail.com>
2024-12-13 01:45:53 +0200

    - Add missing import in main

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:monumento/monument_data_manager.dart';
