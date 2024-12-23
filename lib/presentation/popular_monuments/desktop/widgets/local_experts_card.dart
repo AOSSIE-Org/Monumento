05726416eab37aa62a7753096041f46e90892308
kareem abdeen <kareem.elshreef18@gmail.com>
2024-12-23 02:26:54 +0200

    Aِِِdd local experts text heaader for LocalExpertsCard

--- lib/presentation/popular_monuments/desktop/widgets/local_experts_card.dart
+++ lib/presentation/popular_monuments/desktop/widgets/local_experts_card.dart
@@ -8,7 +8,8 @@ import 'package:url_launcher/url_launcher.dart';
 class LocalExpertsCard extends StatefulWidget {
   final List<LocalExpertEntity> localExperts;
   final double width;
-  const LocalExpertsCard({super.key, required this.localExperts, required this.width});
+  const LocalExpertsCard(
+      {super.key, required this.localExperts, required this.width});
 
   @override
   State<LocalExpertsCard> createState() => _LocalExpertsCardState();
@@ -19,69 +20,87 @@ class _LocalExpertsCardState extends State<LocalExpertsCard> {
   Widget build(BuildContext context) {
     var width = widget.width;
     return Card(
-      child: SizedBox(
-        width: width ,
-        child: ListView.separated(
-          shrinkWrap: true,
-          itemBuilder: (ctx, index) {
-            return ListTile(
-              leading: CircleAvatar(
-                backgroundImage: CachedNetworkImageProvider(
-                  widget.localExperts[index].imageUrl,
-                ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Text(
+              "Local Guides and Experts",
+              style: AppTextStyles.s18(
+                color: AppColor.appSecondary,
+                fontType: FontType.MEDIUM,
+                isDesktop: true,
               ),
-              title: Text(widget.localExperts[index].name),
-              subtitle: Text(
-                widget.localExperts[index].designation,
-              ),
-              trailing: IconButton(
-                icon: const Icon(Icons.phone),
-                onPressed: () async {
-                  if (await canLaunchUrl(
-                    Uri.parse('tel:${widget.localExperts[index].phoneNumber}'),
-                  )) {
-                    launchUrl(
-                      Uri.parse(
-                          'tel:${widget.localExperts[index].phoneNumber}'),
-                    );
-                  } else {
-                    if (mounted) {
-                      showDialog(
-                        context: context,
-                        builder: (context) {
-                          return AlertDialog(
-                            title: Text(
-                                "Contact ${widget.localExperts[index].name}"),
-                            content: Text(
-                                "You can contact ${widget.localExperts[index].name} at ${widget.localExperts[index].phoneNumber}"),
-                            actions: [
-                              TextButton(
-                                onPressed: () {
-                                  Navigator.pop(context);
-                                },
-                                child: Text(
-                                  "Close",
-                                  style: AppTextStyles.s14(
-                                      color: AppColor.appPrimary,
-                                      fontType: FontType.MEDIUM,
-                                      isDesktop: true),
-                                ),
-                              ),
-                            ],
+            ),
+          ),
+          Divider(),
+          SizedBox(
+            width: width,
+            child: ListView.separated(
+              shrinkWrap: true,
+              itemBuilder: (ctx, index) {
+                return ListTile(
+                  leading: CircleAvatar(
+                    backgroundImage: CachedNetworkImageProvider(
+                      widget.localExperts[index].imageUrl,
+                    ),
+                  ),
+                  title: Text(widget.localExperts[index].name),
+                  subtitle: Text(
+                    widget.localExperts[index].designation,
+                  ),
+                  trailing: IconButton(
+                    icon: const Icon(Icons.phone),
+                    onPressed: () async {
+                      if (await canLaunchUrl(
+                        Uri.parse(
+                            'tel:${widget.localExperts[index].phoneNumber}'),
+                      )) {
+                        launchUrl(
+                          Uri.parse(
+                              'tel:${widget.localExperts[index].phoneNumber}'),
+                        );
+                      } else {
+                        if (mounted) {
+                          showDialog(
+                            context: context,
+                            builder: (context) {
+                              return AlertDialog(
+                                title: Text(
+                                    "Contact ${widget.localExperts[index].name}"),
+                                content: Text(
+                                    "You can contact ${widget.localExperts[index].name} at ${widget.localExperts[index].phoneNumber}"),
+                                actions: [
+                                  TextButton(
+                                    onPressed: () {
+                                      Navigator.pop(context);
+                                    },
+                                    child: Text(
+                                      "Close",
+                                      style: AppTextStyles.s14(
+                                          color: AppColor.appPrimary,
+                                          fontType: FontType.MEDIUM,
+                                          isDesktop: true),
+                                    ),
+                                  ),
+                                ],
+                              );
+                            },
                           );
-                        },
-                      );
-                    }
-                  }
-                },
-              ),
-            );
-          },
-          separatorBuilder: (ctx, index) {
-            return const Divider();
-          },
-          itemCount: widget.localExperts.length,
-        ),
+                        }
+                      }
+                    },
+                  ),
+                );
+              },
+              separatorBuilder: (ctx, index) {
+                return const Divider();
+              },
+              itemCount: widget.localExperts.length,
+            ),
+          ),
+        ],
       ),
     );
   }
