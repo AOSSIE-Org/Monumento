6170517a8ca4edaf593aa93f77c995f44f15d922
ShridharAggrawal <agrawalshreedhar8@gmail.com>
2025-04-03 20:03:18 +0530

    fix: Follow button state persistence and real-time updates

--- lib/application/profile/follow/follow_bloc.dart
+++ lib/application/profile/follow/follow_bloc.dart
@@ -5,14 +5,19 @@ import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:monumento/domain/entities/user_entity.dart';
 import 'package:monumento/domain/repositories/social_repository.dart';
+import 'package:monumento/application/authentication/authentication_bloc.dart';
+import 'package:monumento/domain/repositories/authentication_repository.dart';
 
 part 'follow_event.dart';
 part 'follow_state.dart';
 
 class FollowBloc extends Bloc<FollowEvent, FollowState> {
   final SocialRepository _socialRepository;
+  final AuthenticationBloc _authenticationBloc;
+  final AuthenticationRepository _authenticationRepository;
+  bool _currentFollowStatus = false;
 
-  FollowBloc(this._socialRepository) : super(FollowInitial()) {
+  FollowBloc(this._socialRepository, this._authenticationBloc, this._authenticationRepository) : super(FollowInitial()) {
     on<FollowUser>(_mapFollowUserToState);
     on<UnfollowUser>(_mapUnfollowUserToState);
     on<GetFollowStatus>(_mapGetFollowStatusToState);
@@ -23,10 +28,20 @@ class FollowBloc extends Bloc<FollowEvent, FollowState> {
     try {
       emit(LoadingFollowState());
       await _socialRepository.followUser(targetUser: event.targetUser);
-      add(GetFollowStatus(targetUser: event.targetUser));
+      _currentFollowStatus = true;
+      emit(FollowStatusRetrieved(following: _currentFollowStatus));
+      
+      // Update authentication state
+      _authenticationBloc.add(LoggedIn());
+      
+      // Get the current user's following and followers lists
+      final (userLoggedIn, user) = await _authenticationRepository.getUser();
+      if (userLoggedIn && user != null) {
+        add(LoadUser(following: user.following));
+        add(LoadUser(following: user.followers));
+      }
     } catch (e) {
       log('${e.toString()} follow');
-
       emit(FollowStateError(e.toString()));
     }
   }
@@ -34,12 +49,21 @@ class FollowBloc extends Bloc<FollowEvent, FollowState> {
   _mapUnfollowUserToState(UnfollowUser event, Emitter<FollowState> emit) async {
     try {
       emit(LoadingFollowState());
-
       await _socialRepository.unfollowUser(targetUser: event.targetUser);
-      add(GetFollowStatus(targetUser: event.targetUser));
+      _currentFollowStatus = false;
+      emit(FollowStatusRetrieved(following: _currentFollowStatus));
+      
+      // Update authentication state
+      _authenticationBloc.add(LoggedIn());
+      
+      // Get the current user's following and followers lists
+      final (userLoggedIn, user) = await _authenticationRepository.getUser();
+      if (userLoggedIn && user != null) {
+        add(LoadUser(following: user.following));
+        add(LoadUser(following: user.followers));
+      }
     } catch (e) {
       log('${e.toString()} unfollow');
-
       emit(FollowStateError(e.toString()));
     }
   }
@@ -48,12 +72,11 @@ class FollowBloc extends Bloc<FollowEvent, FollowState> {
       GetFollowStatus event, Emitter<FollowState> emit) async {
     try {
       emit(LoadingFollowState());
-      bool following =
+      _currentFollowStatus =
           await _socialRepository.getFollowStatus(targetUser: event.targetUser);
-      emit(FollowStatusRetrieved(following: following));
+      emit(FollowStatusRetrieved(following: _currentFollowStatus));
     } catch (e) {
       log('${e.toString()} status');
-
       emit(FollowStateError(e.toString()));
     }
   }
@@ -71,7 +94,6 @@ class FollowBloc extends Bloc<FollowEvent, FollowState> {
       emit(LoadedFollowUserListState(userData: userDataEntity));
     } catch (e) {
       log('${e.toString()} follow');
-
       emit(FollowUserListErrorState(message: e.toString()));
     }
   }
